024b958d36b7e9bbf5d963b0dafbe77121f5de89
clerk3 <fernalfa@gmail.com>
2023-03-03 16:56:24 -0500

    TNT ODDS version 2.0
    latest released version

--- Rot Number Consecutivo.py
+++ Rot Number Consecutivo.py
@@ -2,20 +2,21 @@ import time
 import pyautogui
 time.sleep(3)
 
+ROTATION = 821001
+ADD = 2
 
-number = 2
-value = 537
 
-
+REPEAT = 20
 def deleteempty():
     pyautogui.press('enter')
-    pyautogui.press('home')
     pyautogui.press('del')
-    pyautogui.typewrite(str(value))
+    pyautogui.typewrite(str(ROTATION))
     pyautogui.press('enter')
     pyautogui.press('down', presses = 3)
 count = 0
 
-while (count < number):
+while (count < REPEAT):
     count = count + 1
+    print(str(ROTATION))
     print(deleteempty())
+    ROTATION = ROTATION + ADD
