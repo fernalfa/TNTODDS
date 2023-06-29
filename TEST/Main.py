607015c1691a6cdc5b61a131ce23011479dc4774
clerk3 <fernalfa@gmail.com>
2023-06-29 01:44:16 -0400

    update

--- TEST/Main.py
+++ TEST/Main.py
@@ -1,104 +1,62 @@
-import re
+import time
+import pyautogui
+time.sleep(3)
 
-# Lines data
-lines_data = """
-Houston Astros
-C. Javier -R
-O 26½
--135
-1978
-St. Louis Cardinals
-M. Mikolas -R
-U 26½
-+105
-08:05 PM
-1955
-Philadelphia Phillies
-A. Nola -R
-O 25
--135
-1956
-Chicago Cubs
-D. Smyly -L
-U 25
-+105
-08:05 PM
-1959
-Detroit Tigers
-J. Wentz -L
-O 28
--115
-1960
-Texas Rangers
-D. Dunning -R
-U 28
--115
-08:10 PM
-1961
-Cleveland Guardians
-L. Allen -L
-O 27
--140
-1962
-Kansas City Royals
-A. Cox-L
-U 27
-+110
-09:38 PM
-1963
-Chicago White Sox
-L. Giolito -R
-O 26
--140
-1964
-Los Angeles Angels
-J. Barria -R
-U 26
-+110
-09:40 PM
-1965
-New York Yankees
-D. German -R
-O 25½
-+105
-1966
-Oakland Athletics
-J. Sears -L
-U 25½
--135
-09:40 PM
-1979
-Tampa Bay Rays
-Z. Eflin -R
-O 27½
--130
-1980
-Arizona Diamondbacks
-Z. Davies -R
-U 27½
-+100
-"""
+data = '''CIN Reds-logoCIN Reds
+at
+BAL Orioles-logoBAL Orioles
+TODAY 7:05PM
+Inning of First Run
 
-# Define the regex pattern
-pattern = r"(O|U)\s+((\d+)(½)?)\n(-?\d+)"
+1st Inning
+−195
+2nd Inning
++285
+3rd Inning
++650
+4th Inning
++1400
+5th Inning
++2500
+6th Inning
++4500
+7th Inning
++5500
+8th Inning
++7000
+9th Inning
++7500
+10th Inning or Later
++8000
+Inning of Last Run
 
-# Find all matches in the lines data
-matches = re.findall(pattern, lines_data)
+9th Inning
++135
+8th Inning
++220
+7th Inning
++550
+10th Inning or Later
++800
+6th Inning
++900
+5th Inning
++1800
+4th Inning
++3000
+3rd Inning
++4500
+2nd Inning
++7500
+1st Inning
++8000'''
 
-# Iterate over the matches and extract the relevant information
-over_price = None
-under_price = None
+lines = data.split('\n')
 
-for match in matches:
-    over_under = match[0]
-    number = match[1]
-    line = match[4]
-
-    if over_under == 'O':
-        over_price = line
-    elif over_under == 'U':
-        under_price = line
-        # Process the Over and Under prices together
-        print(f"Over/Under: {number}")
-        print(f"Over Price: {over_price}")
-        print(f"Under Price: {under_price}\n")
+for i in range(14, 24):
+    inning_line = lines[i].split()
+    value_line = lines[i+1].split()
+    inning = inning_line[0]
+    value = value_line[0]
+    code = f"pyautogui.press('enter')\npyautogui.write('{value}')\npyautogui.press('enter')\npyautogui.press('down')"
+    print(code)
