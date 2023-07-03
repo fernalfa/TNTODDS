5a9f405de10c7984ac2c875d65f8c8de4c0942d6
clerk3 <fernalfa@gmail.com>
2023-07-03 16:30:43 -0400

    ODDS PRINTED FORMAT CHANGED

--- 1.TO MAKE CUT.py
+++ 1.TO MAKE CUT.py
@@ -1,25 +1,22 @@
 import time
-import pandas
 import pyautogui
 
-# Read data from excel
-sport = "Regular MU Create"
-excel_data = pandas.read_excel('Tools.xlsx', sheet_name=sport)
-count = 0
-time.sleep(5)
-# Iterate excel rows till to finish
+# Read player names from text file
+with open('0.INFO', 'r') as file:
+    player_names = file.readlines()
 
-repeat = 14
+time.sleep(3)
 
-for column in excel_data['Row ID'].tolist():
-    pyautogui.typewrite(str(excel_data['PLAYER'][count]))
+for count, player in enumerate(player_names, start=1):
+    player = player.strip()  # Remove leading/trailing whitespace and newline characters
+    pyautogui.typewrite(player)
     pyautogui.write(" - ")
     pyautogui.write(" TO MAKE THE CUT")
     pyautogui.press('tab', presses=3)
-    pyautogui.typewrite(str(excel_data['PLAYER'][count]))
+    pyautogui.typewrite(player)
     pyautogui.write(" YES MAKE CUT")
     pyautogui.press('tab')
-    pyautogui.typewrite(str(excel_data['PLAYER'][count]))
+    pyautogui.typewrite(player)
     pyautogui.write(" NO MAKE CUT")
     pyautogui.hotkey('alt', 'o')
     time.sleep(8)
@@ -27,12 +24,8 @@ for column in excel_data['Row ID'].tolist():
     pyautogui.hotkey('shift', 'tab')
     pyautogui.hotkey('shift', 'tab')
     pyautogui.hotkey('shift', 'tab')
-    # Set counter with the number of Rows
-    if count == repeat - 1:
-        print(count + 1)
-        print(str(excel_data['PLAYER'][count]))
-        print('COMPLETED')
-        break
-    print(count + 1)
-    print(str(excel_data['PLAYER'][count]))
-    count = count + 1
+
+    print(count)
+    print(player)
+
+print('COMPLETED')
