b0102b6d02b3850cd4d30ef6177a39330f2dcc93
clerk1 <fernalfa1988@hotmail.com>
2023-07-08 19:52:42 -0400

    Team Total calculator

--- 1.TNT_ADD_MISSING_PLAYERS.py
+++ 1.TNT_ADD_MISSING_PLAYERS.py
@@ -1,24 +1,29 @@
 import time
+import pandas
 import pyautogui
-time.sleep(3)  # Starting delay
+# Read data from excel
+sport = "Regular MU Create"
+excel_data = pandas.read_excel('Tools.xlsx', sheet_name=sport)
+count = 0
+time.sleep(3)
 
-# Read player info from text file
-with open('0.INFO', 'r') as file:
-    player_info = file.readlines()
+number = 6
 
-# Iterate through player info
-for player in player_info:
-    player = player.strip()  # Remove leading/trailing whitespaces
 
-    pyautogui.typewrite(player)
+# Iterate excel rows till to finish
+for column in excel_data['Row ID'].tolist():
+
+    pyautogui.typewrite(str(excel_data['PLAYER'][count]))
     pyautogui.press('tab')
     pyautogui.typewrite('-150')
     pyautogui.press('tab', presses=2)
     pyautogui.press('enter')
-
-    print(player)
-
-    # Add any additional logic or actions you need here
-    time.sleep(1)  # Add a delay if needed between iterations
-
-
+    # Set counter with the number of Rows
+    if count == number - 1:
+        print(count+1)
+        print(str((excel_data['PLAYER'][count])))
+        print('COMPLETED')
+        break
+    count = count + 1
+    print(count)
+    print(str((excel_data['PLAYER'][count])))
