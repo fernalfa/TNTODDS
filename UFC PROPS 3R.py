5aa00eb7c99b0da16e016953594717dc35ce6241
clerk1 <fernalfa1988@hotmail.com>
2023-07-08 19:47:11 -0400

    Team Total calculator

--- UFC PROPS 3R.py
+++ UFC PROPS 3R.py
@@ -5,8 +5,54 @@ time.sleep(3)
 
 # COPIAR Y PEGAR DE BOOKMAKER
 data = '''
-
-
+Val Woodburn vrs Bo Nickal: Will The Fight Go To Distance?
+Yes
++750
+No
+-1650
+Val Woodburn vrs Bo Nickal: Fight Distance
+Fight ends In Rd-1
+-325
+Fight ends In Rd-2
++540
+Fight ends In Rd-3
++925
+Fight By Decision
++775
+Val Woodburn vrs Bo Nickal: Who Will Win And When?
+Val Woodburn wins in round 1
++2541
+Val Woodburn wins in round 2
++3120
+Val Woodburn wins in round 3
++4580
+Val Woodburn by decisions
++2897
+Bo Nickal wins in round 1
+-477
+Bo Nickal wins in round 2
++626
+Bo Nickal wins in round 3
++1115
+Bo Nickal by decisions
++1166
+Draw
++6624
+Val Woodburn vrs Bo Nickal:Fight Outcome
+Val Woodburn By KO,TKO OR DQ
++1800
+Val Woodburn By Submission
++4050
+Val Woodburn By Decision
++2950
+Bo Nickal By KO,TKO OR DQ
++225
+Bo Nickal By Submission
+-280
+Bo Nickal By Decision
++1200
+Draw
++6950
 '''
 
 numbers = re.findall(r'[+-]?\d{2,}', data)
@@ -22,7 +68,7 @@ for i, num in enumerate(numbers):
     pyautogui.press('enter')
     pyautogui.press('down')
 
-    if i + 1 in [2, 6, 14]:  # Execute the function after 2, 4, and 9 numbers
+    if i + 1 in [2, 6, 15]:  # Execute the function after 2, 4, and 9 numbers
         pyautogui.press('down')
         pyautogui.press('down')
         pyautogui.press('down')
